400b1a64e2ae4f3442a4ba93b71f50148b67f85c
Thanh-cong VO <tvo@e3r5p5.clusters.42paris.fr>
2023-01-11 22:14:44 +0100

    add bonus

---
+++ bonus/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 15:29:50 by tvo               #+#    #+#             */
+/*   Updated: 2023/01/11 21:10:50 by tvo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+#endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_line(char *stash);
+char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+char	*ft_join(char *s1, char *s2);
+void	*ft_calloc(size_t count, size_t sz);
+void	ft_bzero(void *ptr, size_t count);
+int		ft_strchr(char *str, char c);
+int		ft_findline(char *str);
+
+#endif
---
+++ bonus/get_next_line_utils_bonus.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 16:18:54 by tvo               #+#    #+#             */
+/*   Updated: 2023/01/11 15:03:40 by tvo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+void	ft_bzero(void *ptr, size_t count)
+{
+	size_t	i;
+
+	if (!ptr)
+		return ;
+	i = 0;
+	while (i < count)
+	{
+		*(char *)(ptr + i) = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t sz)
+{
+	void	*ptr;
+
+	if (sz && count * sz / sz != count)
+		return (NULL);
+	ptr = malloc(count * sz);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * sz);
+	return (ptr);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+int	ft_findline(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\n')
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_join(char *s1, char *s2)
+{
+	char	*str;
+	int		sizetotal;
+	int		i;
+	int		j;
+
+	sizetotal = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (sizetotal + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j])
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int		ft_strchr(char *str, char c)
+{
+	while (str && *str)
+	{
+		if (*str == c)
+			return (1);
+		else
+			str++;
+	}
+	return (0);
+}
+
+// int main()
+// {
+// 	char str1[] = "hello word!";
+// 	char *ptr;
+
+// 	ptr = ft_strchr(str1, 'l');
+// 	printf("%s", ptr);
+// 	if (ptr)
+// 	{
+// 		printf("\nl se trouve a l'index %ld", ptr - str1);
+// 	}
+// 	else
+// 		printf("\nl se ne trouve pas dans str1!");
+// }
